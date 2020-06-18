606f8f14b1d2dc7ec8990c515ed93bf7c0d99e7a
heeyoungJung <12young24@naver.com>
2020-06-18 10:43:33 +0900

    ch3

---
+++ awesome-single-file/src/components/BigYellowUsername.vue
@@ -0,0 +1,28 @@
+<template>
+  <p class="username">{{ username }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+$size: 50pt;
+$yellow: #dcae1d;
+
+.username {
+  font-size: $size;
+  color: $yellow;
+
+  .icon {
+    color: green;
+  }
+}
+</style>
