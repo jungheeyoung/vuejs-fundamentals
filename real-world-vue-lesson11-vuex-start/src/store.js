06c89cbbc7ba2f583f6af905427c2be65493edf0
heeyoungJung <12young24@naver.com>
2020-06-19 16:09:26 +0900

    chap4-1

--- real-world-vue-lesson11-vuex-start/src/store.js
+++ real-world-vue-lesson11-vuex-start/src/store.js
@@ -1,24 +1,38 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     user: { id: 'abc123', name: 'Adam Jahr' },
-    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
-    todos: [
-      { id: 1, text: '...', done: true },
-      { id: 2, text: '...', done: false },
-      { id: 3, text: '...', done: true },
-      { id: 4, text: '...', done: false }
-    ]  
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    }
+  },
   getters: {
     getEventById: state => id => {
-      return state.todos.find(event => event.id === id)
+      return state.events.find(event => event.id === id)
     }
   }
 })
