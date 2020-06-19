a561324b337a1129521c4483a6f25b43692f8a82
heeyoungJung <12young24@naver.com>
2020-06-19 14:39:04 +0900

    lesson 4

---
+++ real-world-vue-lesson11-vuex-start/src/views/EventCreate.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1> Create an Event, {{ user.name }} </h1>
+    <p> this event was {{ user.id }} </p>
+    <p>{{ getEventById(2) }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+      ...mapGetters( ['getEventById']),
+      ...mapState(['user', 'categories'])
+  }
+}
+</script>
